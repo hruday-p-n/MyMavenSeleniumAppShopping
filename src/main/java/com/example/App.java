15838c19888be2279fe563a7298fe032c5a5c98c
hruday-p-n <hrudaypn2005@gmail.com>
2026-04-14 18:45:53 +0530

    jenkins changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,32 +2,48 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 
 public class App {
+
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://automationexercise.com/product_details/1");
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        
-        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn.btn-default.cart")));
-        addToCartButton.click();
+        WebDriver driver = new ChromeDriver(options);
 
-        WebElement closeModalButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".modal-close")));
-        closeModalButton.click();
+        try {
+            driver.get("https://automationexercise.com/product_details/1");
 
-        WebElement cartLink = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".shopping_cart a")));
-        cartLink.click();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        System.out.println("Product added to the cart successfully.");
+            WebElement addToCartButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.cssSelector(".btn.btn-default.cart")));
+            addToCartButton.click();
 
-        driver.quit();
+            WebElement closeModalButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.cssSelector(".close-modal")));
+            closeModalButton.click();
+
+            WebElement cartLink = wait.until(
+                ExpectedConditions.elementToBeClickable(By.cssSelector(".shopping_cart a")));
+            cartLink.click();
+
+            System.out.println("Product added successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
